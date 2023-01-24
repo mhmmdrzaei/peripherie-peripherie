257fd3ea5e909c09f2af22d076487b6f79667626
Mohammad <mhmmdrzaei@gmail.com>
2023-01-24 17:01:50 -0500

    minor fixes

--- src/templates/WpPage.js
+++ src/templates/WpPage.js
@@ -2,27 +2,28 @@ import React from "react"
 import {graphql} from "gatsby";
 // import { Helmet } from "react-helmet/es/Helmet";
 import Layout from '../components/layout/layout.component'
-import HomePage from '../components/homePage/homePage.component'
+// import HomePage from '../components/homePage/homePage.component'
 
 const PagePageTemplate = ({ data }) => {
   const { wpPage } = data;
-  const pageLayout =()=> {
-   if(wpPage.id ==='cG9zdDoxMTA=') {
-      return (<HomePage />)
-    } else return (
-      <>
-      <h1>{wpPage.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: wpPage.content }} />
+  // const pageLayout =()=> {
+  // //  if(wpPage.id ==='cG9zdDoxMTA=') {
+  // //     return (<HomePage />)
+  // //   } else return (
+  // //     <>
+  // //     <h1>{wpPage.title}</h1>
+  // //     <div dangerouslySetInnerHTML={{ __html: wpPage.content }} />
 
-      </>
+  // //     </>
 
 
-      )
-  }
+  // //     )
+  // }
   return (
    <Layout>
    
-   {pageLayout()}
+   {/* {pageLayout()} */}
+   <h1>test</h1>
 
     </Layout>
   )
