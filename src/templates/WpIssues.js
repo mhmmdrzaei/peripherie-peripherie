00af10fc8e10189dc34f02f767fbf15c52ca480f
Mohammad <mhmmdrzaei@gmail.com>
2023-02-08 18:21:46 -0500

    issue page building

--- src/templates/WpIssues.js
+++ src/templates/WpIssues.js
@@ -1,14 +1,43 @@
 import React from "react"
-import {graphql} from "gatsby";
+import {graphql, Link} from "gatsby";
 // import { Helmet } from "react-helmet/es/Helmet";
 import Layout from '../components/layout/layout.component'
 
 const IssuesPageTemplate = ({ data }) => {
   const { wpIssue } = data;
+  console.log(wpIssue.issuePages.linkArticles)
   return (
    <Layout>
+    <section className="landingPage">
+    <img src={wpIssue.featuredImage.node.mediaItemUrl} alt={wpIssue.featuredImage.node.altText} id={wpIssue.featuredImage.node.id}/>
+    <div className="landingDetails">
+      <h1>{wpIssue.title}</h1>
+      <div  dangerouslySetInnerHTML={{__html: wpIssue.issuePages.issueDetails }} />
+      <div className="issueContributors">
+          <span>Featuring Works By:</span>
+          {wpIssue.issuePages.issueContributors.map(({contributorName}) => {
+              return (
+                  <Link to={contributorName.uri} id={contributorName.id}>{contributorName.title}</Link>
+              )
+          })}
+      </div>
+      <div className="landingIndex">
+        {wpIssue.issuePages.linkArticles.map(({listingOfArticles}) => {
+              return (
+                <ul>
+                  <li id={listingOfArticles.id}>{listingOfArticles.title}</li>
+                </ul>
+              )
+          })}
+      </div>
+      
+    </div>
 
-   <h1>{wpIssue.title}</h1>
+    </section>
+
+    
+
+    
 
     </Layout>
   )
@@ -18,11 +47,165 @@ export default IssuesPageTemplate;
 export const pageQuery = graphql`
 
 query IssuesPageTemplate($id: String) {
-   wpIssue(id: {eq: $id}) {
-      id
-      link
-      title
-    
+  wpIssue(id: {eq: $id}) {
+    id
+    link
+    title
+    featuredImage {
+      node {
+        id
+        mediaItemUrl
+        altText
+      }
+    }
+    issuePages {
+      fieldGroupName
+      issueContributors {
+        contributorName {
+          ... on WpContributor {
+            id
+            uri
+            title
+          }
+        }
+      }
+      issueDetails
+      linkArticles {
+        fieldGroupName
+        listingOfArticles {
+          ... on WpArticle {
+            id
+            title
+            articleFields {
+              contributors {
+                contributorConnect {
+                  ... on WpContributor {
+                    id
+                    title
+                    uri
+                    link
+                  }
+                }
+                fieldGroupName
+              }
+              pageLayout
+              pageLayoutFields {
+                ... on WpArticle_Articlefields_PageLayoutFields_FullWidthTextEditor {
+                  fieldGroupName
+                  fullWidthTextBox
+                }
+                ... on WpArticle_Articlefields_PageLayoutFields_TwoColumnLayout {
+                  columnWidths
+                  fieldGroupName
+                  columnContentLaidOutFromLeftToRight {
+                    ... on WpArticle_Articlefields_PageLayoutFields_TwoColumnLayout_ColumnContentLaidOutFromLeftToRight_Spacer {
+                      fieldGroupName
+                      spacerHeight
+                    }
+                    ... on WpArticle_Articlefields_PageLayoutFields_TwoColumnLayout_ColumnContentLaidOutFromLeftToRight_Image {
+                      fieldGroupName
+                      imageUpload {
+                        altText
+                        id
+                        uri
+                        link
+                      }
+                    }
+                    ... on WpArticle_Articlefields_PageLayoutFields_TwoColumnLayout_ColumnContentLaidOutFromLeftToRight_Text {
+                      fieldGroupName
+                      textInput
+                    }
+                  }
+                }
+                ... on WpArticle_Articlefields_PageLayoutFields_ImageFullWidth {
+                  fieldGroupName
+                  imageFullWidthUpload {
+                    altText
+                    id
+                    link
+                    uri
+                  }
+                }
+                ... on WpArticle_Articlefields_PageLayoutFields_VideoAudioEmbedding {
+                  contentEmbedding
+                  fieldGroupName
+                }
+                ... on WpArticle_Articlefields_PageLayoutFields_AudioFile {
+                  fieldGroupName
+                  fileTypeLowecase
+                  audioFileUpload {
+                    altText
+                    id
+                    link
+                    uri
+                    title
+                  }
+                }
+              }
+              fieldGroupName
+              multiPageLayout {
+                fieldGroupName
+                multiPageLayoutFields {
+                  ... on WpArticle_Articlefields_multiPageLayout_MultiPageLayoutFields_FullWidthTextEditor {
+                    fieldGroupName
+                    fullWidthTextBox
+                  }
+                  ... on WpArticle_Articlefields_multiPageLayout_MultiPageLayoutFields_TwoColumnLayout {
+                    columnWidths
+                    fieldGroupName
+                    columnContentLaidOutFromLeftToRight {
+                      ... on WpArticle_Articlefields_multiPageLayout_MultiPageLayoutFields_TwoColumnLayout_ColumnContentLaidOutFromLeftToRight_Spacer {
+                        fieldGroupName
+                        spacerHeight
+                      }
+                      ... on WpArticle_Articlefields_multiPageLayout_MultiPageLayoutFields_TwoColumnLayout_ColumnContentLaidOutFromLeftToRight_Image {
+                        fieldGroupName
+                      }
+                      ... on WpArticle_Articlefields_multiPageLayout_MultiPageLayoutFields_TwoColumnLayout_ColumnContentLaidOutFromLeftToRight_Text {
+                        fieldGroupName
+                        textInput
+                      }
+                    }
+                  }
+                  ... on WpArticle_Articlefields_multiPageLayout_MultiPageLayoutFields_ImageFullWidth {
+                    fieldGroupName
+                    imageFullWidthUpload {
+                      altText
+                      mediaItemUrl
+                      uri
+                      link
+                    }
+                  }
+                  ... on WpArticle_Articlefields_multiPageLayout_MultiPageLayoutFields_VideoAudioEmbedding {
+                    contentEmbedding
+                    fieldGroupName
+                  }
+                  ... on WpArticle_Articlefields_multiPageLayout_MultiPageLayoutFields_AudioFile {
+                    fieldGroupName
+                    fileTypeLowecase
+                    audioFileUpload {
+                      altText
+                      link
+                      uri
+                      title
+                    }
+                  }
+                }
+                pageLabel
+              }
+            }
+          }
+        }
+      }
+      publicationPdfUpload {
+        altText
+        id
+        link
+        publicUrl
+        title
+        uri
+      }
+    }
   }
 }
 
