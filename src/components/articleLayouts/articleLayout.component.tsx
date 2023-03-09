c55a41d98c0cca157fa034a85ca9133a3ca3a7f5
Mohammad <mhmmdrzaei@gmail.com>
2023-03-09 13:42:14 -0500

    should have done this as i went, turning to tsx now

---
+++ src/components/articleLayouts/articleLayout.component.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import SinglePageArticle from "../singlePageArticle/singlePageArticle.component";
+import MultiPageArticle from "../multiPageArticle/multiPageArticle.component";
+import ContributorsArticles from "../contributorsArticles/contributorsArticles.component";
+import { v4 as uuidv4 } from 'uuid'; 
+
+interface Article {
+    id: string;
+    title: string;
+    articleFields: {
+      pageLayout: string;
+      pageLayoutFields: any;
+      multiPageLayout: any;
+    }
+  }
+  
+  interface Props {
+    listingOfArticles: Article;
+  }
+  
+  const ArticleLayout: React.FC<Props> = ({listingOfArticles}) => {
+    return (
+      <article key={listingOfArticles.id}>
+        <h2>{listingOfArticles.title}</h2>
+        <ContributorsArticles listingOfArticles={listingOfArticles} />
+        <section className="postContent">
+          {
+            listingOfArticles.articleFields.pageLayout === "Single Page" ? (
+              <SinglePageArticle key={uuidv4()} listingOfArticles={listingOfArticles.articleFields.pageLayoutFields} />
+            ) : listingOfArticles.articleFields.pageLayout === "Multi-Page" ? (
+              <MultiPageArticle key={uuidv4()} listingOfArticles={listingOfArticles.articleFields.multiPageLayout} />
+            ) : null
+          }
+        </section>
+      </article>
+    )
+  }
+  
+  export default ArticleLayout;
